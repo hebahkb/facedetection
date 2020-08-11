c56ac38fd9e74048603c400900e42a1b0e35e3af
hebahkb <heba_kaleem@yahoo.com>
2020-08-11 21:49:21 +0300

    Updates

---
+++ precache-manifest.0e21efa8eda885f2b5803cbb5008b22b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f620391d9a90d071243531d6aeb87d3",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "cd47574c074f2be82e9d",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bfbf8c320f3647b1c72e",
+    "url": "/facedetection/static/css/main.891cf737.chunk.css"
+  },
+  {
+    "revision": "cd47574c074f2be82e9d",
+    "url": "/facedetection/static/js/2.20a89174.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.20a89174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbf8c320f3647b1c72e",
+    "url": "/facedetection/static/js/main.081b6475.chunk.js"
+  },
+  {
+    "revision": "99d29fa1a4ca556ebf26",
+    "url": "/facedetection/static/js/runtime-main.7cf075ce.js"
+  }
+]);
